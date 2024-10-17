d2b3d1c1669dd92566cf2e1a97864e86e02fc208
daniel-beck <beckdp01@gmail.com>
2024-10-17 06:25:15 -0400

    edit prop name in User

--- ToolShare.Data/User.cs
+++ ToolShare.Data/User.cs
@@ -4,11 +4,11 @@ namespace ToolShare.Data;
 
 public class User : IdentityUser
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set;}
-    public string AboutMe { get; set; }
-    public string ProfilePhotoUrl { get; set; }
-    public int BelongsToPod { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set;}
+    public string? AboutMe { get; set; }
+    public string? ProfilePhotoUrl { get; set; }
+    public int PodId { get; set; }
     public DateTime CreatedAt { get; } = DateTime.Now;
 
 }
