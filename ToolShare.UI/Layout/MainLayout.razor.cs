bdff0e9f700a8cc870ac0baa27799b97c6d1a907
daniel-beck <beckdp01@gmail.com>
2024-12-06 20:22:16 -0500

    remove unneeded css

--- ToolShare.UI/Layout/MainLayout.razor.cs
+++ ToolShare.UI/Layout/MainLayout.razor.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Http;
 using ToolShare.UI.DTOs;
 using ToolShare.UI.Identity;
 using ToolShare.UI.Services;
